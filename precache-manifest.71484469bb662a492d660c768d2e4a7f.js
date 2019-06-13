fb82426959f0a50178023d17f1091e3349fd7286
shahalihridoy <shahalihridoy@gmail.com>
2019-06-13 02:31:49 -0700

    Updates

---
+++ precache-manifest.71484469bb662a492d660c768d2e4a7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60bf0f060138af13d56eab7d3dfc3a1",
+    "url": "/fiverr/index.html"
+  },
+  {
+    "revision": "b9b10995ad98073db28c",
+    "url": "/fiverr/static/css/main.69d6d191.chunk.css"
+  },
+  {
+    "revision": "febb4ee673596c7326fa",
+    "url": "/fiverr/static/js/2.785940c3.chunk.js"
+  },
+  {
+    "revision": "b9b10995ad98073db28c",
+    "url": "/fiverr/static/js/main.bc430a3c.chunk.js"
+  },
+  {
+    "revision": "310901a3f00b48f4722f",
+    "url": "/fiverr/static/js/runtime~main.9dcab42f.js"
+  }
+]);
